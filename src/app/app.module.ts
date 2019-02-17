8c405121a5c1ab287b1beeaa1b55b4619c9b1ced
Allen Joseph <allen.joseph.v.a@gmail.com>
2019-02-16 20:11:38 -0500

    feat(i18n): set configuration to es-PE locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, LOCALE_ID } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { registerLocaleData } from '@angular/common';
+import localeES from '@angular/common/locales/es-PE';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 
+registerLocaleData(localeES, 'es-PE');
+
 @NgModule({
     bootstrap: [AppComponent],
     imports: [NativeScriptModule, AppRoutingModule, LoginModule],
     declarations: [AppComponent],
+    providers: [{ provide: LOCALE_ID, useValue: 'es-PE' }],
     schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
